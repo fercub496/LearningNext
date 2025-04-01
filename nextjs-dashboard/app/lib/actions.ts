6f42f2947508376af1ac357bd6e9666c9c57dce8
fercuba96 <fercuba96@gmail.com>
2025-03-31 23:50:20 -0500

    lint errors fixed

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -66,7 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string,prevState: State, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
@@ -88,7 +88,9 @@ export async function updateInvoice(id: string,prevState: State, formData: FormD
           WHERE id = ${id}
         `;
     } catch (error) {
+      console.error(error);
       return { message: 'Database Error: Failed to Update Invoice.' };
+      
     }
    
     revalidatePath('/dashboard/invoices');
